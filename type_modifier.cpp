9eae9ddef13315f6f85d7de0b6ae4d80e761ca6b
Sourav Biswal <souravzsn@gmail.com>
2022-11-06 17:02:48 +0530

    type_modifier code

---
+++ type_modifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int numint=69;
+        double numdoub;
+        numdoub=numint;
+        cout<<numdoub;
+
+        //converting double to int
+        //double is haivng more accuracy than float
+        int numint1;
+        double numdoub1=42.33;
+        numint1=numdoub1;
+        cout<<numint;
+}
