42300f9e3dec38350787703038cae9a08b35445a
Sourav Biswal <souravzsn@gmail.com>
2022-11-08 10:54:27 +0530

    array code

--- array.cpp
+++ array.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
+// inserting elements in an array
 int main(){
         int arr[11];
-        for ()
+        for (int i=0; i<=11; i++){
+                arr[i]=i;
+        cout<<arr[i]<<endl;
+        }
+        cout<<arr; //cant cout array at once without loops
+
 }
