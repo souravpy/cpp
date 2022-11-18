1083a00ceabd4a2a93de7990ec767ca77ac089fa
Sourav Biswal <souravzsn@gmail.com>
2022-11-18 15:55:55 +0530

    updated pointer code

---
+++ enum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+// enum 
+enum week{mon, tue, wed, thur ,fri, sat, sun}; //defining enum
+int main(){
+        week today; // enum object
+        today = 0; // assiging value to object
+        cout <<" today : "<<today;
+
+}
