a006e1d26ba3bfe227dcd464cf0a1cd316f7690d
Sourav Biswal <souravzsn@gmail.com>
2022-11-28 00:15:46 +0530

    constructor code

---
+++ constructor.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// constructor code, inside and outside class definition
+class car{
+        public:
+                Print(); //constructor that prints whenever an variable is declared
+
+}
+
+int main(){
+}
